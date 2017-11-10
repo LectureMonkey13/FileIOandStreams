e9e78c7e18167397da16b33c6e7c862a23157643
William Lingard <LingW200@oriel.w-sussex.sch.uk>
2017-11-10 19:01:56 +0000

    Unfinishd update 1

--- FileIOandStreams/Form1.Designer.vb
+++ FileIOandStreams/Form1.Designer.vb
@@ -22,9 +22,65 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.cmdTestFileIO = New System.Windows.Forms.Button()
+        Me.cmdTestStreams = New System.Windows.Forms.Button()
+        Me.cmdQuit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(13, 13)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(259, 199)
+        Me.TextBox1.TabIndex = 0
+        '
+        'cmdTestFileIO
+        '
+        Me.cmdTestFileIO.Location = New System.Drawing.Point(13, 226)
+        Me.cmdTestFileIO.Name = "cmdTestFileIO"
+        Me.cmdTestFileIO.Size = New System.Drawing.Size(75, 23)
+        Me.cmdTestFileIO.TabIndex = 1
+        Me.cmdTestFileIO.Text = "Test File I/O"
+        Me.cmdTestFileIO.UseVisualStyleBackColor = True
+        '
+        'cmdTestStreams
+        '
+        Me.cmdTestStreams.Location = New System.Drawing.Point(95, 226)
+        Me.cmdTestStreams.Name = "cmdTestStreams"
+        Me.cmdTestStreams.Size = New System.Drawing.Size(81, 23)
+        Me.cmdTestStreams.TabIndex = 2
+        Me.cmdTestStreams.Text = "Test Streams"
+        Me.cmdTestStreams.UseVisualStyleBackColor = True
+        '
+        'cmdQuit
+        '
+        Me.cmdQuit.Location = New System.Drawing.Point(197, 226)
+        Me.cmdQuit.Name = "cmdQuit"
+        Me.cmdQuit.Size = New System.Drawing.Size(75, 23)
+        Me.cmdQuit.TabIndex = 3
+        Me.cmdQuit.Text = "Quit"
+        Me.cmdQuit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.cmdQuit)
+        Me.Controls.Add(Me.cmdTestStreams)
+        Me.Controls.Add(Me.cmdTestFileIO)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents cmdTestFileIO As Button
+    Friend WithEvents cmdTestStreams As Button
+    Friend WithEvents cmdQuit As Button
 End Class
--- FileIOandStreams/Form1.vb
+++ FileIOandStreams/Form1.vb
@@ -1,3 +1,80 @@
-﻿Public Class Form1
+﻿Imports System.IO 'imports file I/O functionality
 
+Public Class Form1
+
+#Region "Main Program"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Show()
+        Me.Text = "File Input & Output" 'set form title
+        Me.TextBox1.Font = New Font("Calibri", 12) 'set font face and size
+        Me.TextBox1.ScrollBars = ScrollBars.Vertical 'force vertial scrollbar
+
+    End Sub
+    'Use file I/O methods for read/write
+    'This procedure will create a new text file and populate it with
+    'supplied text (in this case, supplied as part of the procedure
+    'but the text could just as easily come from user input) and then
+    'save the file. The file is then opened in read mode and its contents
+    'displayed in a text box.
+
+    Private Sub TestFileIO()
+
+        'declare and initalise variables
+        Dim filename As String = ""
+        Dim inputText As String = ""
+        Dim inputLine As String = ""
+        Dim filenumber As Integer = 0
+
+        'set the fileName
+        filename = Directory.GetCurrentDirectory() & "\ReadWrite.txt"
+
+        'find an unused file number
+        'you must request an avadiable file number bfore attemptin to open
+        'a file for reading or writing. Each open file is assinged a file number.
+        filenumber = FreeFile()
+
+        'open the text file for output
+        FileOpen(filenumber, filename, OpenMode.Output, OpenAccess.Write)
+
+        'write some lines of text to the file
+        WriteLine(filenumber, "Soup Soup Tasty Soup")
+        WriteLine(filenumber, "Spicy carrot and corriander")
+        WriteLine(filenumber, "Chili chowder")
+        WriteLine(filenumber, "Crouton Crouton")
+        WriteLine(filenumber, "Crunch friends in a liquid broth")
+        WriteLine(filenumber, "I am gespatchio Oh!")
+        WriteLine(filenumber, "I am a summer soup mmmm!")
+        WriteLine(filenumber, "Miso Miso")
+        WriteLine(filenumber, "Fighting in the dojo")
+        WriteLine(filenumber, "Miso Miso")
+        WriteLine(filenumber, "Oritental Prince in the land of soup")
+
+        'close the file
+        FileClose(filenumber)
+
+        'find an unused file number 
+        filenumber = FreeFile()
+
+        'open text file for input
+        FileOpen(filenumber, filename, OpenMode.Input, OpenAccess.Read)
+
+        'read contents of file into temporary variable
+        Do Until EOF(filenumber)
+            inputLine = LineInput(filenumber)
+            inputText += inputLine.Substring(1, inputLine.Length - 2) & vbCrLf
+        Loop
+
+        'close the file
+        FileClose(filenumber)
+
+        'Display contents of the file
+        TextBox1.Text = "Testing File 
+
+
+
+
+
+    End Sub
+#End Region
 End Class
